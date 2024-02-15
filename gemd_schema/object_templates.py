9aba8536b94856f37f59bc5768407adfa4c61de0
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2024-02-15 11:46:24 -0600

    Add object templates

--- gemd_schema/object_templates.py
+++ gemd_schema/object_templates.py
@@ -1,6 +1,10 @@
 from .identifier import identifier
 from .bounds import one_of_bounds
-from .attribute_templates import condition_template, parameter_template
+from .attribute_templates import (
+    condition_template,
+    parameter_template,
+    property_template,
+)
 
 process_template = {
     "$schema": "http://json-schema.org/draft-06/schema#",
@@ -43,14 +47,17 @@ process_template = {
         "conditions": {
             "type": "array",
             "description": "Templates for associated conditions and bounds narrowing the allowable range",
-            "items": [
-                {
-                    "$ref": "#/definitions/condition_template",
-                },
-                {
-                    "$ref": "#/definitions/one_of_bounds",
-                },
-            ],
+            "items": {
+                "type": "array",
+                "items": [
+                    {
+                        "$ref": "#/definitions/condition_template",
+                    },
+                    {
+                        "$ref": "#/definitions/one_of_bounds",
+                    },
+                ],
+            },
         },
         "parameters": {
             "type": "array",
@@ -74,5 +81,137 @@ process_template = {
         "one_of_bounds": one_of_bounds,
         "condition_template": condition_template,
         "parameter_template": parameter_template,
+    },
+}
+
+material_template = {
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "type": "object",
+    "properties": {
+        "uids": {
+            "$ref": "#/definitions/identifier",
+        },
+        "type": {
+            "type": "string",
+            "const": "material_template",
+        },
+        "tags": {
+            "type": "array",
+            "items": {"type": "string", "maxLength": 256},
+            "maxItems": 100,
+        },
+        "name": {
+            "type": "string",
+            "description": "The name of the template",
+            "maxLength": 128,
+        },
+        "description": {
+            "type": "string",
+            "description": "Some text describing what this template is",
+            "maxLength": 32768,
+        },
+        "properties": {
+            "type": "array",
+            "description": "Templates for associated properties and bounds narrowing the allowable range",
+            "items": {
+                "type": "array",
+                "items": [
+                    {
+                        "$ref": "#/definitions/property_template",
+                    },
+                    {
+                        "$ref": "#/definitions/one_of_bounds",
+                    },
+                ],
+            },
+        },
+    },
+    "required": ["name", "type"],
+    "definitions": {
+        "identifier": identifier,
+        "one_of_bounds": one_of_bounds,
+        "property_template": property_template,
+    },
+}
+
+measurement_template = {
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "type": "object",
+    "properties": {
+        "uids": {
+            "$ref": "#/definitions/identifier",
+        },
+        "type": {
+            "type": "string",
+            "const": "measurement_template",
+        },
+        "tags": {
+            "type": "array",
+            "items": {"type": "string", "maxLength": 256},
+            "maxItems": 100,
+        },
+        "name": {
+            "type": "string",
+            "description": "The name of the template",
+            "maxLength": 128,
+        },
+        "description": {
+            "type": "string",
+            "description": "Some text describing what this template is",
+            "maxLength": 32768,
+        },
+        "conditions": {
+            "type": "array",
+            "description": "Templates for associated conditions and bounds narrowing the allowable range",
+            "items": {
+                "type": "array",
+                "items": [
+                    {
+                        "$ref": "#/definitions/condition_template",
+                    },
+                    {
+                        "$ref": "#/definitions/one_of_bounds",
+                    },
+                ],
+            },
+        },
+        "parameters": {
+            "type": "array",
+            "description": "Templates for associated parameters and bounds narrowing the allowable range",
+            "items": {
+                "type": "array",
+                "items": [
+                    {
+                        "$ref": "#/definitions/parameter_template",
+                    },
+                    {
+                        "$ref": "#/definitions/one_of_bounds",
+                    },
+                ],
+            },
+        },
+        "properties": {
+            "type": "array",
+            "description": "Templates for associated properties and bounds narrowing the allowable range",
+            "items": {
+                "type": "array",
+                "items": [
+                    {
+                        "$ref": "#/definitions/property_template",
+                    },
+                    {
+                        "$ref": "#/definitions/one_of_bounds",
+                    },
+                ],
+            },
+        },
+    },
+    "required": ["name", "type"],
+    "definitions": {
+        "identifier": identifier,
+        "one_of_bounds": one_of_bounds,
+        "condition_template": condition_template,
+        "parameter_template": parameter_template,
+        "property_template": property_template,
     },
 }
--- tests/test_object_templates.py
+++ tests/test_object_templates.py
@@ -1,8 +1,16 @@
 # We are going to test the process_template object template
 import jsonschema
-from gemd_schema.attribute_templates import parameter_template
+from gemd_schema.attribute_templates import (
+    parameter_template,
+    property_template,
+    condition_template,
+)
 from gemd_schema.bounds import real_bounds, one_of_bounds
-from gemd_schema.object_templates import process_template
+from gemd_schema.object_templates import (
+    process_template,
+    material_template,
+    measurement_template,
+)
 
 
 def test_process_template():
@@ -72,3 +80,141 @@ def test_process_template():
     }
 
     jsonschema.validate(data, process_template)
+
+
+def test_material_template():
+    data = {
+        "type": "material_template",
+        "uids": {
+            "cookie_templates": "choc_chip",
+            "id": "2e1bec7e-bda4-441d-bebb-1215bfa6ee0f",
+        },
+        "tags": [],
+        "name": "Chocolate Chip Cookie Template",
+        "description": "Template for chocolate chip cookie materials",
+        "properties": [
+            [
+                {
+                    "uids": {"cookie_templates": "choc_chip_comp_01"},
+                    "tags": ["ingredients::cookies::nutsallowed"],
+                    "name": "Chocolate Chip Cookie Composition",
+                    "type": "property_template",
+                    "description": "Specifying the composition of the linked cookie",
+                    "bounds": {
+                        "type": "categorical_bounds",
+                        "categories": [
+                            "flour",
+                            "baking soda",
+                            "baking powder",
+                            "salt",
+                            "butter",
+                            "granulated sugar",
+                            "brown sugar",
+                            "vanilla extract",
+                            "eggs",
+                            "chocolate chips",
+                            "chopped nuts",
+                        ],
+                    },
+                },
+                {
+                    "type": "categorical_bounds",
+                    "categories": [
+                        "flour",
+                        "baking soda",
+                        "baking powder",
+                        "salt",
+                        "butter",
+                        "granulated sugar",
+                        "brown sugar",
+                        "vanilla extract",
+                        "eggs",
+                        "chocolate chips",
+                        "chopped nuts",
+                    ],
+                },
+            ]
+        ],
+    }
+    jsonschema.validate(data, material_template)
+
+
+def test_measurement_template():
+    property1_template = {
+        "uids": {"cookie_templates": "hedonic_index_prop"},
+        "tags": [],
+        "type": "property_template",
+        "name": "Chocolate Chip Cookie Hedonic Index",
+        "description": "The allowable range for the hedonic index of chocolate chip cookies.",
+        "bounds": {
+            "type": "real_bounds",
+            "lower_bound": 9.99,
+            "upper_bound": 10,
+            "default_units": "",
+        },
+    }
+    jsonschema.validate(property1_template, property_template)
+
+    property1_bounds = {
+        "type": "real_bounds",
+        "lower_bound": 9.99,
+        "upper_bound": 10,
+        "default_units": "",
+    }
+    jsonschema.validate(property1_bounds, one_of_bounds)
+
+    condition1_template = {
+        "name": "Cookie Temperature",
+        "uids": {"cookie_templates": "cookie_eating_temp"},
+        "tags": [],
+        "type": "condition_template",
+        "description": "A template for valid temperature ranges for eating cookies.",
+        "bounds": {
+            "default_units": "kelvin",
+            "type": "real_bounds",
+            "lower_bound": 250,
+            "upper_bound": 380,
+        },
+    }
+    jsonschema.validate(condition1_template, condition_template)
+
+    condition1_bounds = {
+        "default_units": "kelvin",
+        "type": "real_bounds",
+        "lower_bound": 250,
+        "upper_bound": 380,
+    }
+
+    parameter1_template = {
+        "name": "Number of Cookies",
+        "uids": {"cookie_templates": "cookie_count"},
+        "tags": ["chocula"],
+        "type": "parameter_template",
+        "description": "A template for the number of cookies to eat for a hedonic index test.",
+        "bounds": {
+            "type": "integer_bounds",
+            "lower_bound": 1,
+            "upper_bound": 1000,
+        },
+    }
+    jsonschema.validate(parameter1_template, parameter_template)
+
+    data = {
+        "type": "measurement_template",
+        "uids": {
+            "cookie_templates": "choc_chip_hedonic",
+            "id": "664e5b79-4c16-46db-aa9e-ff70d79d2f79",
+        },
+        "tags": [],
+        "name": "Chocolate Chip Cookie Hedonic Index Measurement Template",
+        "description": "Template for chocolate chip cookie materials",
+        "properties": [[property1_template, property1_bounds]],
+        "conditions": [[condition1_template, condition1_bounds]],
+        "parameters": [
+            [
+                parameter1_template,
+                {"type": "integer_bounds", "lower_bound": 1, "upper_bound": 1000},
+            ]
+        ],
+    }
+    jsonschema.validate(data, measurement_template)
